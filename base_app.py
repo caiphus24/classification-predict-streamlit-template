ebcb12639a8e2c6d24621d059765d359163096ad
caiphus24 <matibidimgoboya@gmail.com>
2022-06-27 14:43:31 +0200

    improved eda modeling

--- base_app.py
+++ base_app.py
@@ -61,20 +61,12 @@ import matplotlib as mpl
 
 
 import seaborn as sns
-
-
 # Overrall graph sizes
 plt.rcParams['figure.dpi'] = 150
-
-
 # NLP Libraries
 nlp = NLP()
-
-
 # Setting global constants to ensure notebook results are reproducible
-
 PARAMETER_CONSTANT = 'Magic String'
-
 # ignore warnings
 warnings.filterwarnings('ignore')
 
@@ -84,8 +76,7 @@ style.use('seaborn-pastel')
 style.use('seaborn-poster')
 
 
-# ML Libraries
-nlp = NLP()
+
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl", "rb")
@@ -97,30 +88,84 @@ raw = pd.read_csv("resources/train.csv")
 
 # The main function where we will build the actual app
 raw['Analysis'] = [['Negative', 'Neutral', 'Positive', 'News'][x+1]
-                           for x in raw['sentiment']]
+                   for x in raw['sentiment']]
 # Function to extract hashtags from tweets
+
+
 def hashtag_extract(tweet):
 
-	hashtags = []
+    hashtags = []
 
-	for i in tweet:
-		ht = re.findall(r"#(\w+)", i)
-		hashtags.append(ht)
+    for i in tweet:
+        ht = re.findall(r"#(\w+)", i)
+        hashtags.append(ht)
 
-	hashtags = sum(hashtags, [])
-	frequency = nltk.FreqDist(hashtags)
+    hashtags = sum(hashtags, [])
+    frequency = nltk.FreqDist(hashtags)
 
-	hashtag_df = pd.DataFrame(
-		{'hashtag': list(frequency.keys()), 'count': list(frequency.values())})
-	hashtag_df = hashtag_df.nlargest(15, columns="count")
+    hashtag_df = pd.DataFrame(
+        {'hashtag': list(frequency.keys()), 'count': list(frequency.values())})
+    hashtag_df = hashtag_df.nlargest(15, columns="count")
 
-	return hashtag_df
+    return hashtag_df
+
 
 # Extracting the hashtags from tweets in each class
-positive= hashtag_extract(raw['message'][raw['Analysis'] == 'Positve'])
+positive = hashtag_extract(raw['message'][raw['Analysis'] == 'Positive'])
 negative = hashtag_extract(raw['message'][raw['Analysis'] == 'Negative'])
 neutral = hashtag_extract(raw['message'][raw['Analysis'] == 'Neutral'])
 news = hashtag_extract(raw['message'][raw['Analysis'] == 'News'])
+
+#Creating a cleaning data function, class and pipelines 
+
+#cleaning data functions 
+def cleanup_text(input_text: str) -> str:
+    
+    """
+    Apply some basic cleanup to the input text. 
+    input takes the text as a string
+    :param input_text: The input text.
+    :return: The cleaned input text
+    """
+    text = input_text.lower()
+    #changing the input_text to lower case
+    '''Replacing words matching regular expressions
+        : We are looking at the most regualr expressions'''
+    
+    text = re.sub(r"won't", 'will not', text)
+    text = re.sub(r"can't", 'cannot', text)
+    text = re.sub(r"i'm", 'i am', text)
+    text = re.sub(r'ain\'t', 'is not', text)
+    text = re.sub(r"doesn't", 'is does not', text)
+    text = re.sub(r'(\w+)\'ll', '\g<1> will', text)
+    text = re.sub(r'(\w+)n\'t', '\g<1> not', text)
+    text = re.sub(r'(\w+)\'ve', '\g<1> have', text)
+    text = re.sub(r'(\w+)\'s', '\g<1> is', text)
+    text = re.sub(r'(\w+)\'re', '\g<1> are', text)
+    text = re.sub(r'(\w+)\'d', '\g<1> would', text)
+    #Removing the username
+    text= re.sub('@[^\s]+','',text)
+    #Removing the special characters
+    text = re.sub('r<.*?>',' ', text)
+    text = re.sub("^\s+|\s+$", "", text, flags=re.UNICODE)
+    #Removing all the punctuations in the text that we may not need
+    punctuation = re.compile("[.;:!\'’‘“”?,\"()\[\]]")
+    text = punctuation.sub("", text.lower()) 
+
+    '''It is important to not remove all stop_words as they might mess up our data during analysis, we will introduce nlp to keep some of the stop words'''
+    nlp_stopwords = NLP(replace_words=True, remove_stopwords=True, remove_numbers=True, remove_punctuations=False) 
+    text = nlp_stopwords.process(text)
+    #Tokenizing the text
+    text = text.split()
+    # Limmatizing the text with the help of pos tag
+    #Pos tag is a speech tag
+    pos = pos_tag(text)
+    # Only considering words with speech tag
+    lemmatizer = WordNetLemmatizer()
+    text = ' '.join([lemmatizer.lemmatize(word, po[0].lower()) if (po[0].lower() in ['n', 'r', 'v', 'a'] and word[0] != '@') else word for word, po in pos])
+    
+    #returning the cleaned text
+    return text
 
 
 def main():
@@ -167,11 +212,11 @@ def main():
         st.markdown('**Imbalanced Classication**: A classication predictive modeling problem where thedistribution of examples across the classes is not equal.')
         st.subheader(
             'The following visuals help us analyse the most active users based on the sentiment(positive, neutral, news or negative)')
-        
+
         # extract users from the message column
         raw['users'] = [''.join(re.findall(r'@\w{,}', line))
                         if '@' in line else np.nan for line in raw.message]
-        
+
         # count the tweet message per users and group by users
         user_counts = raw[['message', 'users']].groupby(
             'users', as_index=False).count().sort_values(by='message', ascending=False)
@@ -213,13 +258,33 @@ def main():
         st.pyplot(fig)
         st.markdown('The above graphs gives us an idea on who was mentioned or who engaged the most in the climate change, We can therefore assume that the user or users who got mentioned most in all sentiments they are the  leading leaders of the Climate chaneg senate, It would not make sense if it happens that unknown users could have so much engagements if it happens that they hold no position in the senate of the Word Climat Change.')
 
-		# Hashtags per sentiment
+        # Hashtags per sentiment
         st.subheader(
             'The following visuals provides the hashtags per tweets sentiment')
+        # Addjustment figues for the multiple graphs of the hashtag below
+        fig, axy = plt.subplots(ncols=2, nrows=2, figsize=(16, 14), dpi=100)
+        fig.subplots_adjust(wspace=0.4, hspace=0.3, left=0, right=1)
+        # Plotting positive hastags
+        sns.barplot(y=positive['hashtag'], x=positive['count'], palette=(
+            "Blues_d"), ax=axy[0, 0])
+        axy[0, 0].set_title('Frequent Positive climate change hashtags')
+        # Plotting news hashtags
+        sns.barplot(y=news['hashtag'], x=news['count'],
+                    palette=("Blues_d"), ax=axy[0, 1])
+        axy[0, 1].set_title('Frequent News climate change hashtags')
+        # Plotting neutral hashtags
+        sns.barplot(y=neutral['hashtag'], x=neutral['count'],
+                    palette=("Blues_d"), ax=axy[1, 0])
+        axy[1, 0].set_title('Frequent Neutral climate change hashtags')
+        # Plotting negative hashtags
+        sns.barplot(y=negative['hashtag'], x=negative['count'], palette=(
+            "Blues_d"), ax=axy[1, 1])
+        axy[1, 1].set_title('Frequent Negative climate change hashtags', )
+        st.pyplot(fig)
 
-        
+        st.markdown('A hashtag—written with a "#" symbol—is used to index keywords or topics on Twitter. This function was created on Twitter,\
+            and allows people to easily follow topics they are interested in. Hashtags help you to identify the most trending topics at that moment, thus helping us in analysing our data. It is believed Our most #hashtag trends will be based mostly on  Climate change. The above graphs helps us understand which trends had more engagements and if those trends where eithe positive, negative, news or just nuetral. This gives us a better insight of our data. ')
 
-        
     # Building out the predication page
     if selection == "Prediction":
         st.info("Prediction with ML Models")
